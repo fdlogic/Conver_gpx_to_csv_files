f95fcb8064d96865b669de566d59c19c678083ca
fede <fgdangiolo@gmail.com>
2022-10-04 20:46:41 -0300

    Adding files

---
+++ Convert_gpx_to_csv.py
@@ -0,0 +1,58 @@
+from gpx_converter import Converter
+import os
+import argparse
+
+def convert_gpx_to_dictionary(gpx_file):
+    """
+    This function convert the gpx data to a fictionary.
+
+    Arguments:
+    gpx_file: file with gpx data.
+
+    Return:
+    position_data: dictionary with data about latitude, longitude and altitude
+    """
+
+    position_data = Converter(input_file=gpx_file).gpx_to_dictionary()
+
+    return position_data
+
+
+def convert_gpx_to_csv(gpx_file, csv_file):
+    """
+    This function convert the gpx data to a csv file.
+
+    Arguments:
+    gpx_file: file with gpx data.
+
+    Return:
+    pass_result: a bool data that indicate if the converssion was succesfull
+    position_data_csv: csv file with data about latitude, longitude and attitude
+    """
+
+    pass_result = Converter(input_file=gpx_file).gpx_to_csv(output_file=csv_file)
+
+    return pass_result
+
+def get_comline_parser():
+    parser = argparse.ArgumentParser(description='Search files')
+    parser.add_argument("--path", type=str, default = '.', help = "Path where found the files")
+
+    return parser
+
+
+if __name__== "__main__":
+
+    args = get_comline_parser().parse_args()
+
+    for file_ in os.listdir(args.path):
+        name, extension = os.path.splitext(file_)
+        
+        if extension in [".gpx"]:
+            gpx_file = name + extension
+            csv_file = name + ".csv"
+            print("I found the file {}".format(gpx_file))
+
+            pass_result = convert_gpx_to_csv(gpx_file, csv_file)
+            if(pass_result):
+                print("file {} converted to .csv".format(gpx_file))
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Converto_gpx_csv_files
-This repository let you convert a gpx file to a csv file
+# Convert gpx file to a csv file
+Simple python scripts that let you convert a gpx file to a csv file
+
+### Prerequisites 
+For this example, use the next requirements 
+
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+Usage 
+
+```
+python3 Convert_gpx_to_csv.py -h
+usage: Convert_gpx_to_csv.py [-h] [--path PATH]
+
+Search files
+
+optional arguments:
+  -h, --help   show this help message and exit
+  --path PATH  Path where found the files
+```
+
+### Example for convert
+
+```
+python3 Convert_gpx_to_csv.py
+```
+
